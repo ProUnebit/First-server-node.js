0012ce0e4c34092dfcc08e137ee9666ab2819d80
ProUnebit <injuriaverbalis@mail.ru>
2017-05-07 04:07:03 +0300

    add canvas

--- index.js
+++ index.js
@@ -9,34 +9,37 @@ let io = require('socket.io')(http);
 
 var port = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/pages/index.html');
 });
 
-app.get('/chatroom', function(req, res){
+app.get('/chatroom', function(req, res) {
   res.render(__dirname + '/views/pages/chatroom.ejs');
 });
 
-app.get('/motion', function(req, res){
+app.get('/motion', function(req, res) {
   res.render(__dirname + '/views/pages/interface+motion/motion.ejs');
 });
 
-app.get('/interface', function(req, res){
+app.get('/interface', function(req, res) {
   res.render(__dirname + '/views/pages/interface+motion/interface.ejs');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
   console.log('New connect');
-  socket.on('disconnect', function(){
-   console.log('Connection is turned off');
- });
-  socket.on('chat message', function(msg){
+  socket.on('disconnect', function() {
+    console.log('Connection is turned off');
+  });
+  socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
+  socket.on('interface+motion_send', function(data) {
+    socket.broadcast.emit('interface+motion_listen', data);
+  });
 });
 
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('Server started on port: ' + port);
 });
 
