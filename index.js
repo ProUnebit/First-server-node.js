b2f7a7cc3b02e907647489e0e2f0430a54d0c0fb
ProUnebit <injuriaverbalis@mail.ru>
2017-05-08 05:55:44 +0300

    users+enter+arrows

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // var express = require('express');
 // var app = express();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -25,7 +23,7 @@ app.get('/interface', function(req, res) {
   res.render(__dirname + '/views/pages/interface+motion/interface.ejs');
 });
 
-
+var sprites = {};
 
 io.on('connection', function(socket) {
   console.log('New connect');
@@ -36,32 +34,30 @@ io.on('connection', function(socket) {
     io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
+
+  socket.emit('interface+motion_listen', sprites);
   socket.on('interface+motion_send', function(data) {
-    socket.broadcast.emit('interface+motion_listen', data);
+    var axesX = [150, 200, 250, 300, 350, 400, 450];
+    if (socket.id in sprites) {
+      switch (data.command) {
+        case 'right':
+          sprites[socket.id].x += 60;
+          break;
+        case 'left':
+          sprites[socket.id].x -= 60;
+          break;
+      }
+    } else {
+      sprites[socket.id] = {
+        x: 350,
+        y: 280
+      };
+    };
+
+    socket.broadcast.emit('interface+motion_listen', sprites);
   });
 });
 
 http.listen(port, function() {
   console.log('Server started on port: ' + port);
 });
-
-
-// app.set('port', (process.env.PORT || 5000));
-//
-// app.use(express.static(__dirname + '/public'));
-//
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-//
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
-//
-// app.get('/chatroom', function(request, response) {
-//   response.render('pages/chatroom');
-// });
-//
-// app.listen(app.get('port'), function() {
-//   console.log('Server started on port:', app.get('port'));
-// });
