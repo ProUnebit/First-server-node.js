d3f2a8fb2538931e5a2e53d0d317368bf6f34700
ProUnebit <injuriaverbalis@mail.ru>
2017-05-29 05:36:57 +0300

    new pages/interface+motion/epilepsia-mobile.ejs +++

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
 // var express = require('express');
 // var app = express();
 
-var app = require('express')();
-var http = require('http').Server(app);
-let io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/pages/index.html');
 });
 
-app.get('/chatroom', function(req, res) {
+app.get('/chatroom', (req, res) => {
   res.render(__dirname + '/views/pages/chatroom.ejs');
 });
 
-app.get('/showcode', function(req, res) {
+app.get('/showcode', (req, res) => {
   res.render(__dirname + '/views/pages/showcode.ejs');
 });
 
-app.get('/motion', function(req, res) {
+app.get('/motion', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/motion.ejs');
 });
 
-app.get('/interface', function(req, res) {
+app.get('/interface', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/interface.ejs');
 });
 
-app.get('/interface-circle', function(req, res) {
+app.get('/interface-circle', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/interface-circle.ejs');
 });
 
-app.get('/motion2', function(req, res) {
+app.get('/motion2', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/motion2.ejs');
 });
 
-app.get('/epilepsia-circle', function(req, res) {
+app.get('/epilepsia-circle', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/epilepsia-circle.ejs');
 });
 
-app.get('/epilepsia-mobile', function(req, res) {
+app.get('/epilepsia-mobile', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/epilepsia-mobile.ejs');
 });
 
-app.get('/motion-dom', function(req, res) {
+app.get('/motion-dom', (req, res) => {
   res.render(__dirname + '/views/pages/interface+motion/motion-dom.ejs');
 });
 
-var sprites = {};
+const sprites = {};
 
-io.on('connection', function(socket) {
-  console.log('New connect');
-  socket.on('disconnect', function() {
+io.on('connection', (socket) => {
+  console.warn('New connect');
+  socket.on('disconnect', () => {
     console.log('Connection is turned off');
   });
-  socket.on('chat message', function(msg) {
+  socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    console.log(`message: ${msg}`);
   });
 
   socket.emit('interface+motion_listen', sprites);
-  socket.on('interface+motion_send', function(data) {
-    var axesX = [150, 200, 250, 300, 350, 400, 450];
+  socket.on('interface+motion_send', (data) => {
     if (socket.id in sprites) {
       switch (data.command) {
         case 'right':
@@ -70,18 +69,21 @@ io.on('connection', function(socket) {
         case 'left':
           sprites[socket.id].x -= 60;
           break;
+        default:
+      /* code */
+          break;
       }
     } else {
       sprites[socket.id] = {
         x: 350,
-        y: 280
+        y: 280,
       };
-    };
+    }
 
     socket.broadcast.emit('interface+motion_listen', sprites);
   });
 });
 
-http.listen(port, function() {
-  console.log('Server started on port: ' + port);
+http.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
 });
